4dd7f966869aa81a260f1c388f3fafff70c83750
TukangLas21 <19623160@std.stei.itb.ac.id>
2024-10-06 00:13:57 +0700

    initialize project

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include "robot.hpp"
+
+void menu() {
+    std::cout << "Welcome to Battle of Robots!\n";
+    std::cout << "\n";
+
+    std::cout << "Select your action:\n";
+
+    std::cout << "1. Add robot\n";
+    std::cout << "2. Start battle!\n";
+}
+
+
+int main() {
+
+    menu();
+
+
+    return 0;
+}
---
+++ robot.hpp
@@ -0,0 +1,17 @@
+#ifndef ROBOT_H
+#define ROBOT_H
+
+#include <iostream>
+#include <string>
+
+class Robot {
+    public:
+        int robotID;
+        std::string robotName;
+        int robotATK;
+        int robotDEF;
+        int robotHP;
+};
+
+
+#endif
