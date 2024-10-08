92a971b02a6844911688361c31903ff5f1b17a6c
TukangLas21 <19623160@std.stei.itb.ac.id>
2024-10-08 22:03:43 +0700

    main update

--- Soal_2_Aria Judhistira_13523112_Cakru17/main.cpp
+++ Soal_2_Aria Judhistira_13523112_Cakru17/main.cpp
@@ -1,70 +1,12 @@
 #include <iostream>
-#include "../src/robot.hpp"
-#include "../src/battle.hpp"
+#include <vector>
+#include <string>
+#include <thread>
+#include <chrono>
 
-/*** Robot Class Implementation ***/
-class Robot {
-    public:
-        int robotID;
-        std::string robotName;
-        int robotATK;
-        int robotDEF;
-        double robotHP;
-};
+using namespace std;
 
-/* Simplify terms */
-#define name robotName
-#define atk robotATK
-#define def robotDEF
-#define hp robotHP
-
-/*** Battle Class Implementation ***/
-
-class Battle {
-    private:
-        bool battleOver = false; // boolean variable for the battle loop
-
-    public:
-        void start_fight(Robot robotOne, Robot robotTwo) {
-
-            std::srand(static_cast<unsigned>(time(0)));
-
-            std::cout << "Battle start!\n";
-
-            std::cout << robotOne.name << " attacks " << robotTwo.name << " for " << damageBattle(robotOne.atk, robotTwo.def);
-
-            damageHP(damageBattle(robotOne.atk, robotTwo.def), robotTwo.hp);
-            
-            if (robotTwo.hp <= 0) battleOver = true;
-
-            while (!battleOver) {
-                
-
-
-            }
-
-        
-        }
-};
-
-/*** Game Class Implementation ***/
-
-class Game {
-    public:
-        void add_robot() { // Method to add a new robot
-
-        }
-
-        void start_game() {
-            
-        }
-};
-
-/*** Other functions and procedures ***/
-bool validInput(int input) {
-    return input == input;
-}
-
+/* Other functions and procedures */
 double randomNumber() { // rng for attack and defense value, ranging from -0.3 to 0.3
     return (static_cast<double>(std::rand()) / RAND_MAX * 0.6) - 0.3; 
 }
@@ -75,62 +17,153 @@ double damageBattle(int ATK, int DEF) { // returns the damage dealt by the attac
     return damageTaken;
 }
 
-void damageHP(double damage, double HP) { // reduces defending robot's HP
-    HP -= damage;
+// double damageHP(double damage, double HP) { // reduces defending robot's HP
+//     return (HP -= damage);
+// }
+
+class Robot {
+    public:
+        string name;
+        int atk;
+        int def;
+        double hp;
+
+        // Konstruktor
+        Robot(string RobotName, int RobotATK, int RobotDEF, double RobotHP) {
+            name = RobotName;
+            atk = RobotATK;
+            def = RobotDEF;
+            hp = RobotHP;
+        }
+
+        void display() const {
+            std::cout << name <<  "(ATK: "  << atk << " DEF: " << def << " HP: " << hp << ")" << endl;
+        }
+};
+
+class Battle {
+    public:
+        void start_fight(Robot RoboOne, Robot RoboTwo) {
+            double HP1 = RoboOne.hp;
+            double HP2 = RoboTwo.hp;
+
+            while (HP1 > 0 || HP2 > 0) {
+                HP2 -= damageBattle(RoboOne.atk, RoboTwo.def);
+                cout << RoboOne.name << " attacks " << RoboTwo.name << " for " << damageBattle(RoboOne.atk, RoboTwo.def) << " damage!" << endl;
+                this_thread::sleep_for(chrono::milliseconds(1000));
+                HP1 -= damageBattle(RoboTwo.atk, RoboOne.def);
+                cout << RoboTwo.name << " attacks " << RoboOne.name << " for " << damageBattle(RoboTwo.atk, RoboOne.def) << " damage!" << endl;
+                this_thread::sleep_for(chrono::milliseconds(1000));
+            }
+
+            if (HP2 < 0) {
+                cout << RoboTwo.name << " is defeated!" << endl;
+                this_thread::sleep_for(chrono::milliseconds(1000));
+                cout << RoboOne.name << " wins!" << endl;
+            }
+            else if (HP1 < 0) {
+                cout << RoboOne.name << " is defeated!" << endl;
+                this_thread::sleep_for(chrono::milliseconds(1000));
+                cout << RoboTwo.name << " wins!" << endl;
+            }
+        }
+};
+
+class Game {
+    public:
+        void add_robot(vector<Robot> &robot_vec) {
+            cout << "Please fill in the new robot's data!" << endl;
+
+            string RoboName;
+            cout << "Insert the new robot's name: ";
+            cin >> RoboName;
+            cout << endl;
+
+            int RoboAttack;
+            cout << "Insert " << RoboName << "\'s attack power: ";
+            cin >> RoboAttack;
+            cout << endl;
+
+            int RoboDef;
+            cout << "Insert " << RoboName << "\'s defense power: ";
+            cin >> RoboDef;
+            cout << endl;
+
+            double RoboHP;
+            cout << "Insert " << RoboName << "\'s HP: ";
+            cin >> RoboHP;
+            cout << endl;
+
+            robot_vec.push_back(Robot(RoboName, RoboAttack, RoboDef, RoboHP));
+
+            cout << "Robot has been successfully added!" << endl;
+        }
+
+        void start_game(vector<Robot> &robot_vec) {
+            cout << "Choose robots for the battle: " << endl;
+            for (size_t i = 0; i < robot_vec.size(); i++) {
+                cout << (i+1) << ". ";
+                robot_vec[i].display();
+            }
+            
+            cout << "Choose the first Robot : ";
+            int RoboOne;
+            cin >> RoboOne;
+
+            cout << "Choose the second Robot : ";
+            int RoboTwo;
+            cin >> RoboTwo;
+
+            Robot roboOne = robot_vec[RoboOne-1];
+            Robot roboTwo = robot_vec[RoboTwo-1];
+
+            Battle battle;
+            battle.start_fight(roboOne, roboTwo);
+        }
+};
+
+vector<Robot> initialize_robot_vector() {
+    vector<Robot> robot_vec = {
+        Robot("Mip", 500, 30, 700),
+        Robot("Mup", 300, 40, 800),
+        Robot("Mop", 600, 20, 500)
+    };
+
+    return robot_vec;
 }
 
-void initRobots() {
-    Robot Mip;
-    Robot Mup;
-    Robot Mop;
-
-    // Mip attributes
-    Mip.name = "Mip";
-    Mip.atk = 500;
-    Mip.def = 20;
-    Mip.hp = 700;
-
-    // Mup attributes
-    Mup.name = "Mup";
-    Mup.atk = 300;
-    Mup.def = 40;
-    Mup.hp = 800;
-
-    // Mop attributes
-    Mop.name = "Mop";
-    Mop.atk = 600;
-    Mop.def = 30;
-    Mop.hp = 500;
-}
-
-/*** Main program ***/
 int main() {
+    vector<Robot> RoboVector = initialize_robot_vector();
 
     Game NewGame;
-    initRobots();
 
-    std::cout << "Welcome to Battle of Robots!\n";
-    std::cout << "\n";
+    while (true) {
+        cout << "Welcome to Battle of Robots!\n";
+        cout << "\n";
 
-    std::cout << "Select your action:\n";
+        cout << "Select your action:\n";
 
-    std::cout << "1. Add robot\n";
-    std::cout << "2. Start battle!\n";
+        cout << "1. Add robot\n";
+        cout << "2. Start battle!\n";
+        cout << "3. Quit\n";
 
-    int action;
-    do
-    {
-        std::cin >> action;
-    } while (!validInput(action) && action >= 1 && action <= 2);
-    
-    if (action == 1) {
-        NewGame.add_robot();
+        cout << "Choose your action: ";
+        int action;
+        do
+        {
+            cin >> action;
+        } while (action < 1 && action > 3);
+
+        if (action == 1) {
+            NewGame.add_robot(RoboVector);
+        }
+        else if (action == 2) {
+            NewGame.start_game(RoboVector);
+        }
+        else {
+            break;
+        }
     }
-    else {
-        NewGame.start_game();
-    }
-
-
 
     return 0;
 }
